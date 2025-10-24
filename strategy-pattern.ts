a790cc5bb99c3a3f056754d1fe62f3cdf1e0e47e
Hadi <me@habibhadi.com>
2025-10-24 00:11:00 -0600

    wip

--- strategy-pattern.ts
+++ strategy-pattern.ts
@@ -37,8 +37,6 @@ class RubberDuck extends Duck {
 Problems here:
 
 Code duplication: Each duck must override fly and quack.
-
-Rigid behavior: If we want to change behavior at runtime (e.g., a toy duck gets a rocket), we’d have to subclass again.
-
+Rigid behavior: If we want to change behavior at runtime (e.g., a toy duck gets a rocket), we'd have to subclass again.
 Hard to maintain: Adding new behavior means editing multiple classes.
 */
